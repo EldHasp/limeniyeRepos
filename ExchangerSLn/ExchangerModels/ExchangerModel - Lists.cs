6ac77555616b416447192dfb435aa2cae352daaa
limeniye <limeniye@gmail.com>
2021-02-23 22:26:49 +0300

    Added Rates

---
+++ ExchangerSLn/ExchangerModels/ExchangerModel - Lists.cs
@@ -0,0 +1,30 @@
+﻿using DtoTypes;
+using System.Collections.Generic;
+
+namespace ExchangerModels
+{
+    public partial class ExchangerModel
+    {
+        /// <summary>Неизменяемый словарь предложение по обмену базовой валюты.</summary>
+        public IReadOnlyDictionary<RateDto, ExchangeDto> Exchanges { get; }
+
+        /// <summary>Внутреннее поле "Только для чтения" со словарём предложение по обмену базовой валюты.</summary>
+        private readonly Dictionary<RateDto, ExchangeDto> exchanges;
+
+        /// <summary>Неизменяемый словарь доступных, для пар, валют.</summary>
+        private IReadOnlyList<CurrencyDto> availableCurrency { get; } = new List<CurrencyDto>
+        {
+            new CurrencyDto("USD", "$"),
+            new CurrencyDto("EUR", "€"),
+            new CurrencyDto("RUB", "₽"),
+            new CurrencyDto("CNY", "¥"),
+            new CurrencyDto("KZT", "₸"),
+            new CurrencyDto("VND", "₫"),
+            new CurrencyDto("CHF", "₣")
+        };
+
+
+        private List<RateDto> rates;
+        public IReadOnlyList<RateDto> Rates { get; }
+    }
+}
---
+++ ExchangerSLn/ExchangerModels/ExchangerModel.cs
@@ -0,0 +1,91 @@
+﻿using Common.Interfaces.Model;
+using DtoTypes;
+using Repository;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace ExchangerModels
+{
+    public partial class ExchangerModel : IExchangerModel
+    {
+        private IRepository<RateDto> api;
+        private CurrencyDto Base;
+        
+        public Task SetBaseCurrency(CurrencyDto currency)
+        {
+            Base = currency;
+            bool result = false;
+            UpdateRates();
+            UpdateExchangeRates();
+            return null; 
+        }
+
+
+        public ExchangeDto GetExchange(CurrencyDto currency, CurrencyDto @base, decimal amounBase)
+        {
+            RateDto pair = api.GetCurrencyRate(currency, @base).Result;
+            var value = pair.Rate * amounBase;
+            ExchangeDto exchange = new ExchangeDto(pair, amounBase, value, 999);
+            return exchange;
+        }
+
+        public IReadOnlyList<CurrencyDto> GetCurrencies()
+        {
+            return availableCurrency;
+        }
+
+        public Task UpdateRates()
+        {
+            return null;
+        }
+        public Task UpdateExchangeRates()
+        {
+            return null;
+        }
+
+        private async void UpdateRates(int everySeconds, IList<CurrencyDto> availbleCurrenciew, CurrencyDto @base)
+        {
+            while (true)
+            {
+                var task = Task.Run(() => api.GetAllCurrencyRate(@base, availbleCurrenciew));
+                try
+                {
+                    var ratesResult = await task;
+                    foreach (var item in ratesResult)
+                    {
+                        if (rates.Find(x => x.Currency == item.Currency && x.Base == item.Base) != null)
+                            rates.Add(item);
+                        //else
+                        //rates.Find(x => x.Currency == item.Currency && x.Base == item.Base ) = item;\
+
+                        RaiseActionRates(Common.Enums.ActionListEnum.Added, item);
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    throw new ArgumentException(ex.Message);
+                }
+                await Task.Delay(everySeconds * 1000);
+            }
+        }
+
+        public ExchangerModel()
+        {
+            // Инициализация словаря и его неизменяемой оболочки.
+            // Для исходного словаря задаём сравнение строк без учёта регистра
+            exchanges = new Dictionary<RateDto, ExchangeDto>((IEqualityComparer<RateDto>)StringComparer.CurrentCultureIgnoreCase);
+            Exchanges = new ReadOnlyDictionary<RateDto, ExchangeDto>(exchanges);
+
+
+
+            api = new CurrencyExchangerate();
+
+
+            UpdateRates(600, (IList<CurrencyDto>)availableCurrency, Base);
+        }
+    }
+}
