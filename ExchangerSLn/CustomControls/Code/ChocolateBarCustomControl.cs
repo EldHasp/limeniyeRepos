1816220d86305d93170be8837038f5bed38de3b4
limeniye <limeniye@gmail.com>
2021-03-18 22:31:44 +0200

    Добавлен нвоый проект для кастомных элементов. Расписана часть контрола. Есть ошибка с LengthProperty на данном этапе.

--- ExchangerSLn/CustomControls/Code/ChocolateBarCustomControl.cs
+++ ExchangerSLn/CustomControls/Code/ChocolateBarCustomControl.cs
@@ -1,5 +1,4 @@
 ﻿using Common.Interfaces.ViewModel;
-using Exchanger.Styles.Xamls.Code;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,76 +10,53 @@ namespace CustomControls.Code
         #region Public Properties
         /// <summary>Количество строк плиток.
         /// Минимальное значение - одна.</summary>
-        public int Rows
-        {
-            get { return (int)GetValue(RowsProperty); }
-            set { SetValue(RowsProperty, value); }
-        }
+        public int Rows { get { return (int)GetValue(RowsProperty); } set { SetValue(RowsProperty, value); } }
 
         /// <summary>Количество колонок плиток.
         /// Минимальное значение - три.</summary>
-        public int Columns
-        {
-            get { return (int)GetValue(ColumnsProperty); }
-            set { SetValue(ColumnsProperty, value); }
-        }
+        public int Columns { get { return (int)GetValue(ColumnsProperty); } set { SetValue(ColumnsProperty, value); } }
 
         /// <summary>Количество элемнтов одной страницы.</summary>
-        public int Length
-        {
-            get { return (int)GetValue(LengthProperty); }
-            private set { SetValue(LengthPropertyKey, value); }
-        }
+        public int Length { get { return (int)GetValue(LengthProperty); } private set { SetValue(LengthPropertyKey, value); } }
 
         /// <summary>Номер (индекс) страницы.</summary>
-        public int PageIndex
-        {
-            get { return (int)GetValue(PageIndexProperty); }
-            set { SetValue(PageIndexProperty, value); }
-        }
+        public int PageIndex { get { return (int)GetValue(PageIndexProperty); } set { SetValue(PageIndexProperty, value); } }
 
         /// <summary> Все элементы полученные из вне. </summary>
-        public IReadOnlyList<FrameworkElement> AllInputElements { get => (List<FrameworkElement>)GetValue(AllElementsProperty); }
+        public List<object> AllInputElements { private get => (List<object>)GetValue(AllElementsProperty); set { SetValue(AllElementsPropertyKey, value); } }
+
+        /// <summary> Элементы текущей страницы с индексом <see cref="PageIndex"/>. </summary>
+        public IReadOnlyList<object> CurrentPage { get => (List<object>)GetValue(CurrentPageProperty); private set { SetValue(CurrentPagePropertyKey, value); } }
         #endregion
 
-        #region Dependenci Property
-
-        /// <summary><see langword="DependencyProperty"/> для <see cref="Length"/>.</summary>
+        #region DependencyProperty
         public static readonly DependencyProperty LengthProperty = LengthPropertyKey.DependencyProperty;
 
-        /// <summary><see langword="DependencyProperty"/> для <see cref="PageIndex"/>.</summary>
         public static readonly DependencyProperty PageIndexProperty = DependencyProperty.Register(nameof(PageIndex), typeof(int), typeof(ChocolateBarCustomControl), new PropertyMetadata(0, PageIndexChanged, CoercePageIndex));
 
-        /// <summary><see langword="DependencyProperty"/> для <see cref="Rows"/>.</summary>
         public static readonly DependencyProperty RowsProperty = DependencyProperty.Register(nameof(Rows), typeof(int), typeof(ChocolateBarCustomControl), new PropertyMetadata(0, RowsChanged, CoerceRows));
 
-        /// <summary><see langword="DependencyProperty"/> для <see cref="Columns"/>.</summary>
-        public static readonly DependencyProperty ColumnsProperty = DependencyProperty.Register("Columns", typeof(int), typeof(ChocolateBarCustomControl), new PropertyMetadata(0, ColumnsChanged, CoerceColumns));
+        public static readonly DependencyProperty ColumnsProperty = DependencyProperty.Register(nameof(Columns), typeof(int), typeof(ChocolateBarCustomControl), new PropertyMetadata(0, ColumnsChanged, CoerceColumns));
 
-        private static readonly DependencyPropertyKey LengthPropertyKey = DependencyProperty.RegisterReadOnly(nameof(Length), typeof(int), typeof(ChocolateBarCustomControl), new PropertyMetadata(0, LengthChanged));
 
-        /// <summary> Сюда попадают все элементы из вне. </summary>
-        
-
-        /// <summary> DependencyProperty списка входных, стандартных данных </summary>
+        /// <summary> Входной список элементов. </summary>
         public static readonly DependencyProperty AllElementsProperty = AllElementsPropertyKey.DependencyProperty;
 
+        /// <summary> Страница с индексом <see cref="PageIndex"/>. </summary>
+        public static readonly DependencyProperty CurrentPageProperty = CurrentPagePropertyKey.DependencyProperty;
+        #endregion
+
+        #region DependencyPropertyKey
+        private static readonly DependencyPropertyKey LengthPropertyKey = DependencyProperty.RegisterReadOnly(nameof(Length),
+            typeof(int), typeof(ChocolateBarCustomControl), new PropertyMetadata(0, LengthChanged));
+
+        /// <summary> Список текущей страницы с индексом <see cref="PageIndex"/> </summary>
+        private static readonly DependencyPropertyKey CurrentPagePropertyKey = DependencyProperty.RegisterReadOnly(nameof(AllInputElements), 
+            typeof(List<ICellViewModel>), typeof(ChocolateBarCustomControl), new FrameworkPropertyMetadata(new List<ICellViewModel>()));
+
+        /// <summary> Список текущей страницы с индексом <see cref="PageIndex"/> </summary>
         private static readonly DependencyPropertyKey AllElementsPropertyKey = DependencyProperty.RegisterReadOnly(nameof(AllInputElements),
-            typeof(List<object>),
-            typeof(ChocolateBarCustomControl),
-            new FrameworkPropertyMetadata(new List<object>()));
-
-
-        /// <summary> DependencyProperty страницы с индексом <see cref="PageIndex"/>. </summary>
-        public static readonly DependencyProperty CurrentIndexPageProperty = CurrentIndexPagePropertyKey.DependencyProperty;
-
-        /// <summary> DependencyProperty списка страницы и <see cref="AllElementsProperty"/> с индексом <see cref="PageIndex"/> </summary>
-        private static readonly DependencyPropertyKey CurrentIndexPagePropertyKey = DependencyProperty.RegisterReadOnly(nameof(AllInputElements),
-            typeof(List<ICellViewModel>),
-            typeof(ChocolateBarCustomControl),
-            new FrameworkPropertyMetadata(new List<ICellViewModel>()));
-
-
+            typeof(List<object>), typeof(ChocolateBarCustomControl), new FrameworkPropertyMetadata(new List<object>()));
         #endregion
 
         #region Coerces
@@ -115,6 +91,8 @@ namespace CustomControls.Code
         private static void LengthChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             // Переинициализация свойства-коллекции с данными плиток
+            ChocolateBarCustomControl customControl = (ChocolateBarCustomControl)d;
+            customControl.Length = (int)e.NewValue;
         }
         private static void PageIndexChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
@@ -136,5 +114,6 @@ namespace CustomControls.Code
             // и заполняем коллекцию данных плиток актуальной страницы
         }
         #endregion
+    
     }
 }
