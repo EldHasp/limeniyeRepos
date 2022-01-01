a2d70123e14b78823cabdc743c5a31a67c544455
limeniye <limeniye@gmail.com>
2022-01-01 23:40:26 +0200

    Добавлен Uno-проект

---
+++ DragPositionDemonstrateProjectSln/DragPositionUnoProject/DragPositionUnoProject/DragPositionUnoProject.Shared/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Controls;
+
+namespace DragPositionUnoProject
+{
+    public class SomeType : OnNotifyPropertyChanged
+    {
+        private double _positionX, _positionY;
+
+        public double PositionX { get => _positionX; set => SetProperty(ref _positionX, value); }
+        public double PositionY { get => _positionY; set => SetProperty(ref _positionY, value); }
+    }
+
+    public class SomeViewModel : OnNotifyPropertyChanged
+    {
+        public ObservableCollection<SomeType> Types { get; } = new ObservableCollection<SomeType>()
+        {
+            new SomeType() { PositionX = 10, PositionY = 60},
+            new SomeType() { PositionX = 280, PositionY = 60}
+        };
+    }
+
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            DataContext = new SomeViewModel();
+        }
+    }
+}
